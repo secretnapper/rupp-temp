db7a220325bcb6684db8e7c4cf4f74d3529c6eb8
Menard Ysrael Cruz <menard.cruz@smartpaymentplan.com>
2022-02-14 10:25:17 +0800

    added no result output

--- main.js
+++ main.js
@@ -21,18 +21,20 @@ $(function() {
   var nowYear  = new Date().getFullYear();
   var diffYear = nowYear - 2018;
 
-  $('#status').html(`
+  $('#disclaimer').html(`
     <small>
-      <p>DISCLAIMER: This site has not been updated and contains contents ${diffYear} year${diffYear == 1? '': 's'} ago. The comments may not reflect the instructors' and the professors' way of teaching today.</p> 
+      <p>DISCLAIMER: Not affiliated with the original Profs to Pick developer. This site has not been updated and contains contents ${diffYear} year${diffYear == 1? '': 's'} ago. The comments may not reflect the instructors' and the professors' way of teaching today.</p> 
     </small>
   `)
+
+  $('#status').remove();
 })
 
 function StarCalculate(number){
   var starct = parseInt(number);
   var string = "";
   for (var i = 0; i < starct; i++) {
-    string = string + "<i class='fas fa-star'></i>";
+    string = string + "<i class='fa-solid fa-star'></i>";
   }
 
   return string;
@@ -40,7 +42,7 @@ function StarCalculate(number){
 
 function Divider(number){
   var result = "";
-  if((number%5) == 4){
+  if((number%4) == 3){
     result = "<div class='divider d-block'></div>";
   }
   return result;
@@ -59,48 +61,59 @@ function Over(number){
 function mainFunc() {
   $('#search-bar').on('change keyup pase', function() {
     searchName = this.value
-    if (!searchName) return;
-    searchName = searchName.toLowerCase()
-    searchResults = professors.filter(function(p) {
-      firstName = p.firstName.toLowerCase()
-      lastName = p.lastName.toLowerCase()
-      return firstName.includes(searchName)
-        || lastName.includes(searchName) ||
-        (lastName + ' ' + firstName).includes(searchName) ||
-        (lastName + ', ' + firstName).includes(searchName) ||
-        (firstName + ' ' + lastName).includes(searchName)
-    })
-    $('#profs').html('')
-    //searchResults.length
-    for (var i = 0; i < searchResults.length; i++) {
-      $('#profs').append(profTemplate.format(
-        searchResults[i].lastName,
-        searchResults[i].firstName,
-        Over(searchResults[i].rating.helpfulness.toFixed(2)),
-        Over(searchResults[i].rating.pedagogy.toFixed(2)),
-        Over(searchResults[i].rating.easiness.toFixed(2)),
-        Over(searchResults[i].rating.overall.toFixed(2)),
-        searchResults[i]._id,
-        Divider(i),
-      ))
+    
+    if(searchName == ''){
+      $('#profs').html('')
     }
+    else{
+      searchName = searchName.toLowerCase()
+      searchResults = professors.filter(function(p) {
+        firstName = p.firstName.toLowerCase()
+        lastName = p.lastName.toLowerCase()
+        return firstName.includes(searchName)
+          || lastName.includes(searchName) ||
+          (lastName + ' ' + firstName).includes(searchName) ||
+          (lastName + ', ' + firstName).includes(searchName) ||
+          (firstName + ' ' + lastName).includes(searchName)
+      })
+      $('#profs').html('')
+      //searchResults.length
+      if(searchResults.length == 0){
+        $('#profs').append(noResultTemplate);
+      }
+      else{
+        for (var i = 0; i < searchResults.length; i++) {
+          $('#profs').append(profTemplate.format(
+            searchResults[i].lastName,
+            searchResults[i].firstName,
+            Over(searchResults[i].rating.helpfulness.toFixed(2)),
+            Over(searchResults[i].rating.pedagogy.toFixed(2)),
+            Over(searchResults[i].rating.easiness.toFixed(2)),
+            Over(searchResults[i].rating.overall.toFixed(2)),
+            searchResults[i]._id,
+            Divider(i),
+          ))
+        }
+      }
+    }
+    
     $('button').click('click', function() {
       if ($(this).attr('shown') == 'true') {
         reviewList = $(this).parent().children('ul')
         reviewList.html('')
         $(this).attr('shown', 'false')
         $(this).text('Show Reviews')
-        $("#profs li").addClass("d-block")
+        $("#profs li").addClass("d-flex")
         $("#profs li").removeClass("d-none")
          $(".divider").addClass("d-block")
         $(".divider").removeClass("d-none")
       } else {
-        $("#profs li").removeClass("d-block")
+        $("#profs li").removeClass("d-flex")
         $("#profs li").addClass("d-none")
         $(".divider").removeClass("d-block")
         $(".divider").addClass("d-none")
         $(this).parent().removeClass("d-none")
-        $(this).parent().addClass("d-block")
+        $(this).parent().addClass("d-flex")
         reviewList = $(this).parent().children('ul')
         reviewList.html('')
         id = $(this).attr('id')
@@ -127,18 +140,19 @@ function mainFunc() {
   })
 }
 
-profTemplate = '\
-<li class="d-block">\
-  <div>{0}, {1}</div>\
-  <div>Helpfulness: {2}</div>\
-  <div>Pedagogy: {3}</div>\
-  <div>Easiness: {4}</div>\
-  <div>Overall: {5}</div>\
-  <button id="{6}" class="review-button btn">Show Reviews</button>\
-  <ul></ul>\
-</li>\
-{7}\
-'
+profTemplate = `
+<li class="d-flex flex-column">
+  
+  <div>{0}, {1}</div>
+  <div>Helpfulness: {2}</div>
+  <div>Pedagogy: {3}</div>
+  <div>Easiness: {4}</div>
+  <div>Overall: {5}</div>
+  <button id="{6}" class="review-button btn">Show Reviews</button>
+  <ul></ul>
+</li>
+{7}`
+
 reviewTemplate = '\
 <li>\
   <div>{0}</div>\
@@ -149,3 +163,6 @@ reviewTemplate = '\
   <div>Year: {5}</div>\
 </li>\
 '
+noResultTemplate = `
+  <p class="my-3">No results found</p>
+`
